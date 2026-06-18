1d2e004b73a457280159b07de844787bb0ce0689
ra007965-lib <007965@libertas.edu.br>
2026-06-18 20:52:21 -0300

    Delete sala/tividade_06_sala.py

--- sala/tividade_06_sala.py
+++
@@ -1,8 +0,0 @@
-lista = []
-for i in range(5):
-    nomes = input("Digite os nomes: ")
-    lista.append(nomes)
-lista2 = lista[ : :-1]
-print(f"lista 1: {lista}")
-print("\n")
-print(f"lista 2: {lista2}")
